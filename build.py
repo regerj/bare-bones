ac1956892d72d3dfc532abaf11bac193045379d0
regerj <regerjacob@gmail.com>
2023-10-21 21:13:47 -0700

    Updated build script and and tooling

--- build.py
+++ build.py
@@ -1,42 +1,54 @@
-import subprocess, argparse
+#!/usr/bin/python3
 
-from enum import Enum
+import subprocess, argparse, os, shutil
 
-class Command(Enum):
-    ASSEMBLE = 0
-    COMPILE = 1
-    LINK = 2
-    VERIFY_MULTIBOOT = 3
-    CREATE_IMAGE = 4
-    RUN_IMAGE = 5
+from enum import StrEnum
 
-class CommandFactory:
-    def __init__(self, commandType):
-        if commandType is Command.ASSEMBLE:
-            self.commandArray = [{
-                "command": "i686-elf-as",
-                "file": "boot.s",
-                "output": ""
-            }]
-        elif commandType is Command.COMPILE:
-            self.commandArray = [{
-                "command": "i686-elf-gcc",
-                "component_flag": "-c",
-                "file": "",
-                "output_flag": "-o",
-                "output": "",
-                "standard": "-std=gnu99",
-                "freestanding": "-ffreestanding",
-                "optimization": "-O2",
-                "wall": "-Wall",
-                "wextra": "-Wextra"
-            }]
-        elif commandType is Command.LINK:
-            self.commandArray = [{
-                "command": "i686-elf-gcc",
-                "t": "-T",
-                ""
-            }]
+class Command(StrEnum):
+    BUILD = "build"
+    RUN = "run"
+    CLEAN = "clean"
+    
+def Clean():
+    try:
+        shutil.rmtree("build")
+    except:
+        pass
+    
+    try:
+        shutil.rmtree("isodir")
+    except:
+        pass
+
+def Build():
+    Clean()
+    
+    os.mkdir("build")
+    
+    os.chdir("build")
+    output = subprocess.run([
+        "cmake",
+        ".."
+    ])
+    
+    print(output.stdout)
+    
+    output = subprocess.run(
+        "make"
+    )
+    
+    print(output.stdout)
+    
+    os.chdir("..")
+    
+def Run():
+    Build()
+    
+    os.mkdir("isodir")
+    os.mkdir("isodir/boot")
+    os.mkdir("isodir/boot/grub")
+    shutil.copy("./build/bin/timberline", "./isodir/boot")
+    shutil.copy("./src/grub.cfg", "./isodir/boot/grub")
 
 def GetArgs():
     parser = argparse.ArgumentParser(
@@ -62,10 +74,16 @@ def GetArgs():
     
     return parser.parse_args()
 
-def Build():
-    output = subprocess.run("i686-elf-as")
-
 def main():
     args = GetArgs()
     
+    if args.command == Command.BUILD:
+        Build()
+    elif args.command == Command.RUN:
+        Run()
+    else:
+        print("Invalid command")
+        
+if __name__ == "__main__":
+    main()
     
